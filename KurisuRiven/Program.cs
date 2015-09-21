910ee67b2ce9242bc95788dc7c2a527c7fcde082
rexyrexy <miraculousprox@gmail.com>
2015-09-22 00:04:31 +0300

    Kurisu Riven Fix

---
+++ KurisuRiven/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Reflection;
+using LeagueSharp.Common;
+
+namespace KurisuRiven
+{
+    internal static class Program
+    {
+        public static System.Version Version;   
+        private static void Main(string[] args)
+        {
+            Version = Assembly.GetExecutingAssembly().GetName().Version;
+            new KurisuRiven();
+        }
+    }
+}
